986fea9d25070a1cac745b07e72b0d9ea73a2509
Firmansyah Yanuar <fyfirman@gmail.com>
2022-03-28 12:12:51 +0700

    feat: auto reload nginx

--- generate-config.sh
+++ generate-config.sh
@@ -13,5 +13,7 @@ fi
 sh cert-generator.sh $1
 sh nginx-conf-generator.sh $1 $2
 sudo ln -s /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled/$1
+sudo nginx -t
+sudo systemctl reload nginx
 
 echo "Config is successfully generated"
