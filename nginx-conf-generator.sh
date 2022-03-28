30886f777f1323080313a2f2a5767fd416e2df6c
Firmansyah Yanuar <fyfirman@gmail.com>
2022-03-28 12:06:56 +0700

    feat: nginx proxy pass

--- nginx-conf-generator.sh
+++ nginx-conf-generator.sh
@@ -5,11 +5,11 @@ OUTPUT_DIR=/etc/nginx/sites-available/
 generate_site_config() {
   mkdir $OUTPUT_DIR
   OUT=$OUTPUT_DIR$1
-  echo generating config for $1 in $OUT
-  IN=nginx-conf.template
-  SITE=$1 eval "cat <<EOF
+  echo "Generating config for $1 (Port: $2) in $OUT..."
+  IN=nginx-proxy-pass.template
+  SITE=$1 PORT=$2 eval "cat <<EOF
   $(cat $IN)
-  EOF" > $OUT
+EOF" > $OUT
 }
 
-generate_site_config $1
+generate_site_config $1 $2
